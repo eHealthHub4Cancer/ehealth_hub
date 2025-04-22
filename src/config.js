24b1d453f9cf91c2913f8cf50f0219af099db959
DavidIkechi <daakwuru@gmail.com>
2025-04-22 20:43:07 +0100

    .

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
     firebase: {
-      apiKey: "AIzaSyB9omkTlFN1eRgPRcC6Pe6wYR4umG9v_7s",
-      authDomain: "ehealth-hub.firebaseapp.com",
-      projectId: "ehealth-hub",
-      storageBucket: "ehealth-hub.firebasestorage.app",
-      messagingSenderId: "193859337748",
-      appId: "1:193859337748:web:adc6281f367ff3e4147101",
-      measurementId: "G-5T6F8TNT5K"
+      apiKey: "YOUR_API_KEY",
+      authDomain: "YOUR_AUTH_DOMAIN",
+      projectId: "YOUR_PROJECT_ID",
+      storageBucket: "YOUR_STORAGE_BUCKET",
+      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+      appId: "YOUR_APP_ID",
+      measurementId: "YOUR_MEASUREMENT_ID"
     }
   };
   
